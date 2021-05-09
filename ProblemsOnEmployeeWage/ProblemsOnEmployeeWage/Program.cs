ddc81f1b7f83c3826a5abe5ef6131a60c2b98a85
Ketha Charan <charan.ketha1747@gmail.com>
2021-05-09 12:55:35 -0700

    [Charan] Add: DailyWage Of Employee

--- ProblemsOnEmployeeWage/ProblemsOnEmployeeWage/Program.cs
+++ ProblemsOnEmployeeWage/ProblemsOnEmployeeWage/Program.cs
@@ -7,12 +7,19 @@ namespace ProblemsOnEmployeeWage
         static void Main(string[] args)
         {
             int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HR = 20;
+            int empHr = 0;
+            int empWage = 0;
+
             Random random = new Random();
             int empCheck = random.Next(0, 2);
             if (empCheck == IS_FULL_TIME)
-                Console.WriteLine("Employee is Present");
+                empHr = 8;
             else
-                Console.WriteLine("Employee is Absent");
+                empHr = 0;
+
+            empWage = empHr * EMP_RATE_PER_HR;
+            Console.WriteLine("The DailyWage of Employee is :" + empWage);
 
         }
     }
